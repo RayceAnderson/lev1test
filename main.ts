b193d403a793214adc43bd7ceab14edecd583292
RayceAnderson <89491544+RayceAnderson@users.noreply.github.com>
2021-09-19 15:31:38 -0700

    Added reset button, battery sprite, and pre placed blocks.

--- main.ts
+++ main.ts
@@ -1,38 +1,104 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     curBlock.y -= 16;
+if (selectorPanel == true) {
+        curBlock.setImage(assets.image`4BlockV`)
+        curBlock.y = 76
+        vertical = true
+        selectorPanel = false
+        bottomOverlayTest.setImage(assets.image`bottomOverlay`)
+    }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (vertical == true) {
+    if (vertical == true && selectorPanel == false) {
         curBlock.setImage(assets.image`4BlockH`)
         vertical = false
-    } else {
+    } else if (selectorPanel == false) {
         curBlock.setImage(assets.image`4BlockV`)
         vertical = true
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (vertical) {
+    if (vertical && selectorPanel == false) {
         testBlock = sprites.create(assets.image`4BlockV`, SpriteKind.Player)
-    } else {
+        testBlock.z = 1
+        testBlock.x = curBlock.x
+        testBlock.y = curBlock.y
+        placedBlocks.push(testBlock)
+    } else if (selectorPanel == false) {
         testBlock = sprites.create(assets.image`4BlockH`, SpriteKind.Player)
+        testBlock.z = 1
+        testBlock.x = curBlock.x
+        testBlock.y = curBlock.y
+        placedBlocks.push(testBlock)
     }
-    testBlock.x = curBlock.x
-    testBlock.y = curBlock.y
+    if (selectorPanel == true) {
+        if (selectedButton == 1) {
+            for (let i = 0; i <= placedBlocks.length - 1; i++) {
+                placedBlocks[i].destroy()
+            }
+        }
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    curBlock.x -= 16;
+    if (selectorPanel == false) {
+        curBlock.x -= 16;
+    } else {
+        bottomOverlayTest.setImage(assets.image`bottomOverlay1`)
+        selectedButton = 1
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    curBlock.x += 16
+    if (selectorPanel == false) {
+        curBlock.x += 16
+    } else {
+        bottomOverlayTest.setImage(assets.image`bottomOverlay2`)
+        selectedButton = 2
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    curBlock.y += 16
+    if (vertical == true) {
+        if (curBlock.y + 16 < 90) {
+            curBlock.y += 16
+        } else {
+            curBlock.setImage(assets.image`empty`)
+            selectorPanel = true
+            selectedButton = 2
+            bottomOverlayTest.setImage(assets.image`bottomOverlay2`)
+        }
+    } else {
+        if (curBlock.y + 16 < 106) {
+            curBlock.y += 16
+        } else {
+            curBlock.setImage(assets.image`empty`)
+            selectorPanel = true
+            selectedButton = 2
+            bottomOverlayTest.setImage(assets.image`bottomOverlay2`)
+        }
+    }
 })
+let prePlacedBlocks = true
+let placedBlocks: Sprite[] = []
 let testBlock: Sprite = null
+let selectorPanel = false
+let bottomOverlayTest: Sprite = null
 let vertical = false
+let selectedButton = 0
 let curBlock: Sprite = null
+selectedButton = 2
+let battery = sprites.create(assets.image`batteryBlock`, 0)
+let goal = sprites.create(assets.image`goalThing`, 0)
+battery.x = 21
+battery.y = 58
+goal.x = 149
+goal.y = 58
 vertical = true
 curBlock = sprites.create(assets.image`4BlockV`, SpriteKind.Player)
+bottomOverlayTest = sprites.create(assets.image`bottomOverlay`, SpriteKind.Player)
+bottomOverlayTest.z = 100
 curBlock.x = 8
 curBlock.y = 28
+curBlock.z = 1;
 scene.setBackgroundImage(assets.image`bg`)
+if (prePlacedBlocks === true) {
+    let blockOverlay = sprites.create(assets.image`placedBlocksOverlay`, 0)
+}
